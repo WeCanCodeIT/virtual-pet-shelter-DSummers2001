1d29b7a7150e518cd76884d12f443c5fd417d13f
DSummers2001 <dillon.summers2001@gmail.com>
2023-12-11 19:07:49 -0800

    added in tick method/other methods copied do while

--- app/src/main/java/virtual/pet/shelter/Pets.java
+++ app/src/main/java/virtual/pet/shelter/Pets.java
@@ -3,18 +3,18 @@ package virtual.pet.shelter;
 public class Pets {
     String name;
     String description;
-    int thirst = 100;
-    int hunger = 100;
-    int boredom = 100;
+    int thirst = 50;
+    int hunger = 50;
+    int boredom = 50;
     double cage = 10.0;
 
 
     public Pets(String name, String description) {
         this.name = name;
         this.description = description;
-        this.thirst = 100;
-        this.hunger = 100;
-        this.boredom = 100;
+        this.thirst = 50;
+        this.hunger = 50;
+        this.boredom = 50;
         this.cage = 10.0;
     }
 
@@ -76,6 +76,38 @@ public class Pets {
 
     public void setCage(double cage) {
         this.cage = cage;
+    }
+
+    // methods to change values
+    public void changeHunger(int hunger){
+        this.hunger += hunger;
+    }
+
+    public void changeThirst(int thirst){
+        this.thirst += thirst;
+    }
+
+    public void changeBoredom(int boredom){
+        this.boredom += boredom;
+    }
+
+    public void changeCage(double cage){
+        this.cage += cage;
+    }
+
+    public void tick(){
+        this.hunger -= 5;
+        this.thirst -= 5;
+        this.boredom -= 8;
+        this.cage -= 1.0;
+    }
+
+    public void selfPreservation(){
+        if ((this.hunger <= 10) && (this.hunger <= this.thirst)){
+            this.hunger += 5;
+        } else {
+            this.thirst += 5;
+        }
     }
 
 }
--- app/src/main/java/virtual/pet/shelter/Shelter.java
+++ app/src/main/java/virtual/pet/shelter/Shelter.java
@@ -5,27 +5,47 @@ import java.util.Map;
 
 public class Shelter {
     // create a map
-    private Map<Integer, Pets> PetsIn = new HashMap<>();
+    private Map<Integer, Pets> petsIn = new HashMap<>();
     private Map<Integer, Pets> adoptedPets = new HashMap<>();
     // take in a pet
     public void intakePet(int kennelNum, String name, String description) {
         Pets newPetIn = new Pets(name, description);
-        PetsIn.put(kennelNum, newPetIn);
+        petsIn.put(kennelNum, newPetIn);
     }
     // adopt a pet
     public void petAdoption(int kennelNum){
-        adoptedPets.put(kennelNum, PetsIn.get(kennelNum));
-        PetsIn.remove(kennelNum);
+        adoptedPets.put(kennelNum, petsIn.get(kennelNum));
+        petsIn.remove(kennelNum);
     }
     // cage cleanliness
+    public void cleanKennel(){
+        for (Map.Entry<Integer, Pets> entry : petsIn.entrySet()) {
+            int key = entry.getKey();
+            Pets pet = entry.getValue();
+
+            if (pet.getCage() < 10.0){
+                System.out.println("You cleaned kennel number: " + key);
+                pet.changeCage(+4.0);
+            } else {
+                System.out.println(key + " is already clean");
+            }
+        }
+    }
     
     // display all pets in the shelter
     public void shelterPets(){
-        for (Map.Entry<Integer, Pets> entry : PetsIn.entrySet()){
+        if (!petsIn.isEmpty()){
+            System.out.println("Pets in the shelter with kennel number");
+        }
+        for (Map.Entry<Integer, Pets> entry : petsIn.entrySet()){
+            int key = entry.getKey();
+            Pets pet = entry.getValue();
 
+            System.out.println("Kennel number: " + key + " Name: " + pet.getName() + " Description: " + pet.getDescription() + " Kennel cleanliness: " + pet.getCage());
+            System.out.println("Thirst: " + pet.getThirst() + " Hunger: " + pet.getHunger() + " Boredom: " + pet.getBoredom() + "\n");
         }
     }
     // select a certain pet
-
     // display selected pets information
+
 }
